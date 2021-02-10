5153a36a45c040012f275344df002dc4f6c1f6c4
ODL_User 302866 <odl_user_302866@msazurelabs.onmicrosoft.com>
2021-02-10 20:05:43 +0000

    Finish the ExportLicensePlates function

--- TollBooth/DatabaseMethods.cs
+++ TollBooth/DatabaseMethods.cs
@@ -38,11 +38,16 @@ namespace TollBooth
 
             using (_client = new DocumentClient(new Uri(_endpointUrl), _authorizationKey))
             {
+
+                licensePlates = _client.CreateDocumentQuery<LicensePlateDataDocument>(collectionLink,
+                    new FeedOptions() { EnableCrossPartitionQuery = true, MaxItemCount = 100 })
+                    .Where(l => l.exported == false)
+                .ToList();
                 // MaxItemCount value tells the document query to retrieve 100 documents at a time until all are returned.
                 // TODO 5: Retrieve a List of LicensePlateDataDocument objects from the collectionLink where the exported value is false.
                 // COMPLETE: licensePlates = _client.CreateDocumentQuery ...
                 // TODO 6: Remove the line below.
-                licensePlates = new List<LicensePlateDataDocument>();
+                //licensePlates = new List<LicensePlateDataDocument>();
             }
 
             exportedCount = licensePlates.Count();
